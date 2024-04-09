b31ee609d64b236db91ffac8b89bbc564c5e775b
Mustafa khan <19485230-Mustafakhan28@users.noreply.replit.com>
2024-04-09 22:57:37 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+/*Stacks! */
+
+// functions : Push , Pop , Peek , Length
+//Push() : add an element to the top of the stack
+//Pop() : removes the last element of the stack 
+//Peek() : returns the last element of the stack
+//Length() : returns the length of the stack
+
